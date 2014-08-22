41a2ba571e77c7f4a4a96d5a29d222fd401d9583
Fred Shequine <fshequin@gmail.com>
2014-08-22 19:52:50 -0400

    added soil plugin and looked over a view settings and slight style change
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- wp-content/themes/boot-theme/index.php
+++ wp-content/themes/boot-theme/index.php
@@ -21,7 +21,7 @@
 <div class="container">
       <div class="row">
         <div class="col-md-12 header">
-          Header
+          <h1><?php bloginfo('name'); ?></h1>
         </div>
       </div>
     </div>
@@ -29,7 +29,13 @@
 <div class="container">
       <div class="row">
         <div class="col-md-12 main-content">
-          Main Content
+          <h2>Main Content</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
     </div>
@@ -37,7 +43,7 @@
 <div class="container">
       <div class="row">
         <div class="col-md-12 footer">
-          Footer
+          <h3>Footer</h3>
         </div>
       </div>
     </div>
