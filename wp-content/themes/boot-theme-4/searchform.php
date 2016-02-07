e2563a72ddbfa964173b22f86b24ecfcdf186518
Fred Shequine <fshequin@gmail.com>
2016-02-07 13:39:09 -0500

    more bootstrap 4 tweaks
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- wp-content/themes/boot-theme-4/searchform.php
+++ wp-content/themes/boot-theme-4/searchform.php
@@ -2,10 +2,10 @@
 <p>&nbsp;</p>
 <form role="search" method="get" class="form-inline search-form" action="<?php echo home_url( '/' ); ?>">
 	
-	<div class="form-group">
+	<div class="input-group input-group-sm">
 		<label>
 			<span class="screen-reader-text"><?php echo _x( '', 'label' ) ?></span></label>
-			<input type="search" class="form-control input-sm search-field" placeholder="<?php echo esc_attr_x( 'Search ', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+			<input type="search" class="form-control search-field" placeholder="<?php echo esc_attr_x( 'Search ', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 		
 		<!-- <input type="submit" class="btn btn-default search-submit" value="<?php //echo esc_attr_x( 'Search', 'submit button' ) ?>" /> -->
 		<button type="submit" class="btn btn-default btn-sm search-submit"><?php echo esc_attr_x( 'Search', 'submit button' ) ?></button>
