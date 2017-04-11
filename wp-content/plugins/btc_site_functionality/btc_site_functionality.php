d175d15d35b770b7d4956fa84e5417d49e8e4bb0
Fred Shequine <fshequin@gmail.com>
2017-04-11 17:17:42 -0400

    Added BTC Site Functionality plugin
    for turning on custom post types, custom taxonomies, and cmb2 meta boxes and related content addon for cmb2.
    
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

---
+++ wp-content/plugins/btc_site_functionality/btc_site_functionality.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name:       BTC Functionality Plugin
+ * Plugin URI:        http://btcny.com/btc-site-functionality/
+ * Description:       This plugin is used to modularize non-theme specific functionality for your Wordpress site. Custom Post Types, Custom Taxonomies, CMB2 metaboxes. Always leave this plugin activated.
+ * Version:           1.0.0
+ * Author:            Fred Shequine
+ * Author URI:        http://btcny.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       btc_site_functionality
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( !defined( 'WPINC' ) ) {
+	die;
+}
+
+// link in the lib files
+
+	//require WP_PLUGIN_DIR.'/btc_site_functionality/libs/post_types.php';
+	//require WP_PLUGIN_DIR.'/btc_site_functionality/libs/taxonomies.php';
+
+	//add_action('init', 'require_boot_metaboxes');  // old cmb class
+	add_action('init', 'require_boot_cmb2_metaboxes'); // new cmb2 class
+
+	/**
+	 * Initialize the old CMB metabox class.
+	 */
+	function require_boot_metaboxes() {
+	    require WP_PLUGIN_DIR.'/btc_site_functionality/libs/metaboxes.php';
+	    
+	    if( !class_exists('cmb_Meta_Box'))
+	    {
+	        require WP_PLUGIN_DIR.'/btc_site_functionality/libs/metabox/init.php';
+	    }
+	}
+	/**
+	 * Initialize the CMB2 metabox class.
+	 */
+
+	function require_boot_cmb2_metaboxes() {	
+		require WP_PLUGIN_DIR.'/btc_site_functionality/libs/metaboxes_cmb2.php';
+		require WP_PLUGIN_DIR.'/btc_site_functionality/libs/theme_options_cmb2.php'; // uncomment to activate
+	}
+
+	// cmb2 plugin - cmb2-attached-posts-field
+
+	require WP_PLUGIN_DIR.'/btc_site_functionality/libs/cmb2-attached-posts-example.php'; // uncomment to activate
