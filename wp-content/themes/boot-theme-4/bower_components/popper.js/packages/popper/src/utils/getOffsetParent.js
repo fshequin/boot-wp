bbeec397253c654ba2886bd261093c821867decf
Fred Shequine <fshequin@gmail.com>
2019-01-14 15:46:30 -0500

    after bootstrap update
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- wp-content/themes/boot-theme-4/bower_components/popper.js/packages/popper/src/utils/getOffsetParent.js
+++ wp-content/themes/boot-theme-4/bower_components/popper.js/packages/popper/src/utils/getOffsetParent.js
@@ -1,4 +1,5 @@
 import getStyleComputedProperty from './getStyleComputedProperty';
+import isIE from './isIE';
 /**
  * Returns the offset parent of the given element
  * @method
@@ -7,22 +8,29 @@ import getStyleComputedProperty from './getStyleComputedProperty';
  * @returns {Element} offset parent
  */
 export default function getOffsetParent(element) {
-  // NOTE: 1 DOM access here
-  const offsetParent = element && element.offsetParent;
-  const nodeName = offsetParent && offsetParent.nodeName;
-
-  if (!nodeName || nodeName === 'BODY' || nodeName === 'HTML') {
-    if (element) {
-      return element.ownerDocument.documentElement
-    }
-
+  if (!element) {
     return document.documentElement;
   }
 
-  // .offsetParent will return the closest TD or TABLE in case
+  const noOffsetParent = isIE(10) ? document.body : null;
+
+  // NOTE: 1 DOM access here
+  let offsetParent = element.offsetParent || null;
+  // Skip hidden elements which don't have an offsetParent
+  while (offsetParent === noOffsetParent && element.nextElementSibling) {
+    offsetParent = (element = element.nextElementSibling).offsetParent;
+  }
+
+  const nodeName = offsetParent && offsetParent.nodeName;
+
+  if (!nodeName || nodeName === 'BODY' || nodeName === 'HTML') {
+    return element ? element.ownerDocument.documentElement : document.documentElement;
+  }
+
+  // .offsetParent will return the closest TH, TD or TABLE in case
   // no offsetParent is present, I hate this job...
   if (
-    ['TD', 'TABLE'].indexOf(offsetParent.nodeName) !== -1 &&
+    ['TH', 'TD', 'TABLE'].indexOf(offsetParent.nodeName) !== -1 &&
     getStyleComputedProperty(offsetParent, 'position') === 'static'
   ) {
     return getOffsetParent(offsetParent);
