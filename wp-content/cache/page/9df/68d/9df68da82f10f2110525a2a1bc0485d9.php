813aac802673c95740215886f582195fdeee5da9
Fred Shequine <fshequin@gmail.com>
2019-03-08 17:13:04 -0500

    change favorite caching plugin add w3 total cache
    remove wp super cache
    
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

---
+++ wp-content/cache/page/9df/68d/9df68da82f10f2110525a2a1bc0485d9.php
@@ -0,0 +1,93 @@
+|ô‚\<?php exit; ?>a:4:{i:404;b:0;s:7:"headers";a:4:{s:11:"Status-Code";i:200;s:12:"Content-Type";s:25:" text/html; charset=UTF-8";i:0;a:2:{s:1:"n";s:4:"Link";s:1:"v";s:52:" <http://boot-wp/wp-json/>; rel="https://api.w.org/"";}i:1;a:2:{s:1:"n";s:4:"Link";s:1:"v";s:33:" <http://boot-wp/>; rel=shortlink";}}s:4:"time";i:1552082540;s:7:"content";s:10525:"<!DOCTYPE html><html
+lang=en-US prefix="og: http://ogp.me/ns#"><head><meta
+charset=utf-8><meta
+name=viewport content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta
+name=description content><meta
+name=author content><link
+rel=icon href=http://boot-wp/wp-content/themes/boot-theme-4/images/favicon.ico><title>Home - Boot-WP</title><link
+href=http://boot-wp/wp-content/themes/boot-theme-4/bower_components/bootstrap/dist/css/bootstrap.min.css rel=stylesheet><link
+href=//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css rel=stylesheet><link
+href=http://boot-wp/wp-content/themes/boot-theme-4/style.css rel=stylesheet>
+<!--[if lt IE 9]> <script src=https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js></script> <script src=https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js></script> <![endif]--><meta
+name=robots content=noindex,follow><meta
+property=og:locale content=en_US><meta
+property=og:type content=website><meta
+property=og:title content="Home - Boot-WP"><meta
+property=og:description content="Lorem ipsum tellus dictum donec sem massa pharetra, laoreet elementum pulvinar augue morbi in, interdum suspendisse porta diam metus interdum. ligula porttitor vehicula proin consectetur est, placerat eu potenti sollicitudin ac vehicula, potenti consequat class ut. auctor diam id imperdiet sit phasellus pretium, primis donec blandit varius nisi purus, porttitor metus gravida senectus convallis. Donec &hellip;"><meta
+property=og:url content=http://boot-wp/><meta
+property=og:site_name content=Boot-WP><meta
+name=twitter:card content=summary><meta
+name=twitter:description content="Lorem ipsum tellus dictum donec sem massa pharetra, laoreet elementum pulvinar augue morbi in, interdum suspendisse porta diam metus interdum. ligula porttitor vehicula proin consectetur est, placerat eu potenti sollicitudin ac vehicula, potenti consequat class ut. auctor diam id imperdiet sit phasellus pretium, primis donec blandit varius nisi purus, porttitor metus gravida senectus convallis. Donec [&hellip;]"><meta
+name=twitter:title content="Home - Boot-WP"> <script type=application/ld+json>{"@context":"https://schema.org","@type":"WebSite","@id":"http://boot-wp/#website","url":"http://boot-wp/","name":"Boot-WP","potentialAction":{"@type":"SearchAction","target":"http://boot-wp/?s={search_term_string}","query-input":"required name=search_term_string"}}</script> <link
+rel=dns-prefetch href=//cdnjs.cloudflare.com><link
+rel=dns-prefetch href=//s.w.org><link
+rel=alternate type=application/rss+xml title="Boot-WP &raquo; Feed" href=http://boot-wp/feed/><link
+rel=alternate type=application/rss+xml title="Boot-WP &raquo; Comments Feed" href=http://boot-wp/comments/feed/> <script>window._wpemojiSettings={"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/boot-wp\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.1"}};!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55358,56760,9792,65039],[55358,56760,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);</script> <style>img.wp-smiley,img.emoji{display:inline !important;border:none !important;box-shadow:none !important;height:1em !important;width:1em !important;margin:0
+.07em !important;vertical-align:-0.1em !important;background:none !important;padding:0
+!important}</style><link
+rel=stylesheet id=wp-block-library-css  href='http://boot-wp/wp-includes/css/dist/block-library/style.min.css?ver=5.1' type=text/css media=all> <script src='http://boot-wp/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script> <script src='http://boot-wp/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script> <link
+rel=https://api.w.org/ href=http://boot-wp/wp-json/><link
+rel=shortlink href=http://boot-wp/><link
+rel=alternate type=application/json+oembed href="http://boot-wp/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fboot-wp%2F"><link
+rel=alternate type=text/xml+oembed href="http://boot-wp/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fboot-wp%2F&#038;format=xml"></head><body
+class="home page-template-default page page-id-7"><div
+class=container><div
+class=row><div
+class="col-md-12 header"><h1 class="site-title"><a
+href=/>Boot-WP</a></h1><h2 class="site-description">Just another WordPress site</h2><nav
+class="navbar navbar-expand-lg navbar-light">
+<a
+class="nav-title d-sm-block d-lg-none ml-auto" href=#>menu</a><button
+class="navbar-toggler collapsed" type=button data-toggle=collapse data-target=#navbarSupportedContent aria-controls=navbarSupportedContent aria-expanded=false aria-label="Toggle navigation">
+<span
+class=navbar-toggler-icon></span>
+</button><div
+class="collapse navbar-collapse" id=navbarSupportedContent><ul
+id=menu-main_menu class="navbar-nav mr-auto" style="list-style-type: none;"><li
+id=menu-item-13 class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-7 current_page_item first-menu-item menu-item-13 nav-item active"><a
+href=http://boot-wp/ class="nav-link active">Home</a></li><li
+id=menu-item-29 class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29 nav-item"><a
+href=http://boot-wp/about/ class=nav-link>About</a></li><li
+id=menu-item-5 class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5 nav-item dropdown"><a
+href=http://boot-wp/sample-page/ class="nav-link dropdown-toggle" data-toggle=dropdown>Sample Page</a><div
+class=dropdown-menu>
+<a
+href=http://boot-wp/sample-page/ class=" dropdown-item">Sample Page</a></div></li><li
+id=menu-item-12 class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12 nav-item"><a
+href=http://boot-wp/news/ class=nav-link>News</a></li><li
+id=menu-item-28 class="menu-item menu-item-type-post_type menu-item-object-page last-menu-item menu-item-28 nav-item"><a
+href=http://boot-wp/contact/ class=nav-link>Contact</a></li></ul></div>
+</nav></div></div></div><div
+class=container><div
+class=row><div
+class="col-md-8 main-column"><div
+class=main><h3 class="post-title"><a
+href=http://boot-wp/>Home</a></h3><p>Lorem ipsum tellus dictum donec sem massa pharetra, laoreet elementum pulvinar augue morbi in, interdum suspendisse porta diam metus interdum. ligula porttitor vehicula proin consectetur est, placerat eu potenti sollicitudin ac vehicula, potenti consequat class ut. auctor diam id imperdiet sit phasellus pretium, primis donec blandit varius nisi purus, porttitor metus gravida senectus convallis.</p><blockquote
+class=blockquote><p>Donec aliquam velit tristique molestie himenaeos gravida velit augue, dapibus aliquet ad ultrices semper nostra nullam libero fermentum, mattis ullamcorper non porta maecenas donec morbi. sit fusce gravida curae aliquam primis mauris id venenatis conubia, molestie purus facilisis dictum phasellus consequat nulla torquent adipiscing, sed augue donec a quisque dapibus aliquam ultricies.&nbsp;</p></blockquote><p>Rhoncus dictum tortor netus neque enim himenaeos senectus turpis, hendrerit nec nulla justo risus adipiscing enim ultrices, lacus lectus fusce ornare eleifend ante lobortis.</p></div></div><div
+class="col-md-4 sidebar-column"><div
+id=search-4 class="widget widget_search"><form
+role=search method=get class="form-inline search-form" action=http://boot-wp/><div
+class="input-group input-group-sm">
+<label
+for=search1 class=sr-only>Search</label>
+<input
+type=search id=search1 class="form-control search-field" placeholder="Search " value name=s title="Search for:">
+<button
+type=submit class="btn btn-secondary btn-sm search-submit">Search</button></div></form></div><div
+id=pages-2 class="widget widget_pages"><h4 class="widget-title">Pages</h4><ul><li
+class="page_item page-item-23"><a
+href=http://boot-wp/about/>About</a></li><li
+class="page_item page-item-26"><a
+href=http://boot-wp/contact/>Contact</a></li><li
+class="page_item page-item-7 current_page_item"><a
+href=http://boot-wp/ aria-current=page>Home</a></li><li
+class="page_item page-item-10"><a
+href=http://boot-wp/news/>News</a></li><li
+class="page_item page-item-2"><a
+href=http://boot-wp/sample-page/>Sample Page</a></li></ul></div></div></div></div><div
+class=container><div
+class=row><div
+class="col-md-12 footer-column"><p>&copy;2019 - Boot-WP - (Bootstrap 4) - <a
+href=/wp-admin/ target=_blank><span
+class=d-none>admin link</span><i
+class="fa fa-cog"></i></a></p></div></div></div> <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js?ver=1.12.9'></script> <script src='http://boot-wp/wp-content/themes/boot-theme-4/bower_components/bootstrap/dist/js/bootstrap.min.js?ver=4.0.0'></script> <script src='http://boot-wp/wp-content/themes/boot-theme-4/js/site.min.js?ver=1.0'></script> <script src='http://boot-wp/wp-includes/js/wp-embed.min.js?ver=5.1'></script> </body></html>";}
